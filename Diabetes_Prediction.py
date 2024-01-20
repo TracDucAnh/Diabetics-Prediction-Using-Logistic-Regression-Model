0ca3085443fcece90a268ae06d3588bcc3dcb850
Dinh Trac Duc Anh <141731906+TracDucAnh@users.noreply.github.com>
2024-01-20 23:38:21 +0700

    Update Diabetes_Prediction.py

--- Diabetes_Prediction.py
+++ Diabetes_Prediction.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 
 # import data
 df = pd.read_csv("dataset/diabetes2.csv")
