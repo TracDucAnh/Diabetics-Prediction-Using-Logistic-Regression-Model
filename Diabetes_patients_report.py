ce88b39b9107865e707d6f028af52c0256d07554
dinhtracducanh <dinhtracducanh04092005@gmail.com>
2024-02-01 23:00:03 +0700

    fixed

--- Diabetes_patients_report.py
+++ Diabetes_patients_report.py
@@ -1,5 +1,6 @@
 from ydata_profiling import ProfileReport
-import pandas as pd df = pd.read_csv("dataset/diabetes2.csv")
+import pandas as pd 
+df = pd.read_csv("dataset/diabetes2.csv")
 profile = ProfileReport(df, title = "Diabetes patients report")
 profile.to_file("Diabetes _patients_report.html")
 
